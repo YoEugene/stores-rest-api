cf55772e4c12e2bdecc3a0b31b0007e6ab968f79
YoEugene <yoeugene.hsu@gmail.com>
2019-04-22 15:57:52 +0800

    Add User resource get/delete endpints

--- app.py
+++ app.py
@@ -5,20 +5,28 @@ from flask_restful import Api
 from flask_jwt import JWT
 
 from security import authenticate, identity as identity_function
-from resources.user import UserRegister
+from resources.user import UserRegister, User
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 from datetime import timedelta
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db') # can change to MySQL, PostgreSQL etc
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL', 'sqlite:///data.db')  # can change to MySQL, PostgreSQL etc
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'eugene'
 api = Api(app)
 
-jwt = JWT(app, authenticate, identity_function) # create an endpoint: /auth
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+jwt = JWT(app, authenticate, identity_function)  # create an endpoint: /auth
+
 
 @jwt.auth_response_handler
 def customized_response_handler(access_token, identity):
@@ -26,21 +34,26 @@ def customized_response_handler(access_token, identity):
         'access_token': access_token.decode('utf-8'),
         'user_id': identity.id
     })
-@jwt.jwt_error_handler
-def customized_error_handler(error):
-    return jsonify({
-        'message': error.description,
-        'code': error.status_code
-    }), error.status_code
+
+
+# @jwt.jwt_error_handler
+# def customized_error_handler(error):
+#     return jsonify({
+#         'message': error.description,
+#         'code': error.status_code
+#     }), error.status_code
+
 
 app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
 app.config['JWT_AUTH_USERNAME_KEY'] = 'email'
 
 api.add_resource(Item, '/item/<string:name>')  # http://127.0.0.1/item/macbook
-api.add_resource(Store, '/store/<string:name>')  # http://127.0.0.1/item/macbook
+# http://127.0.0.1/item/macbook
+api.add_resource(Store, '/store/<string:name>')
 api.add_resource(ItemList, '/items')  # http://127.0.0.1/items
 api.add_resource(StoreList, '/stores')  # http://127.0.0.1/items
 api.add_resource(UserRegister, '/register')
+api.add_resource(User, '/user/<int:user_id>')
 
 if __name__ == '__main__':
     from db import db
--- resources/user.py
+++ resources/user.py
@@ -2,19 +2,19 @@ import sqlite3
 from flask_restful import Resource, reqparse
 from models.user import UserModel
 
-class UserRegister(Resource):
 
+class UserRegister(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('username',
-        type=str,
-        required=True,
-        help="This field cannot be left blank!"
-    )
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
     parser.add_argument('password',
-        type=str,
-        required=True,
-        help="This field cannot be left blank!"
-    )
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
 
     def post(self):
         data = UserRegister.parser.parse_args()
@@ -26,3 +26,20 @@ class UserRegister(Resource):
         user.save_to_db()
 
         return {"message": "User created successfully."}, 201
+
+
+class User(Resource):
+    @classmethod
+    def get(cls, user_id):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {'message': 'User not found.'}, 404
+        return user.json()
+
+    @classmethod
+    def delete(cls, user_id):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {'message': 'User not found.'}, 404
+        user.delete_from_db()
+        return {'message': 'User deleted'}
